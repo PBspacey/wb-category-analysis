4f52f1c1e5b9381118d1de88859a21349a827379
Никита Сеняткин <nikitasenyatkin@MacBook-Air-Nikita-5.local>
2024-05-28 00:26:48 +0300

    ml inference connected with the html

--- app/api.py
+++ app/api.py
@@ -1,4 +1,13 @@
 from flask import Flask, request, render_template, redirect, url_for
+from models.models import Doc2VecModel, CBclassifier, CVmodel
+import os
+
+
+current_directory = os.getcwd()
+print("Текущая рабочая директория:", current_directory)
+
+new_directory = "."
+os.chdir(new_directory)
 
 
 app = Flask(__name__)
@@ -17,6 +26,48 @@ def about_page():
 @app.route('/evaluate')
 def evaluate_page():
     return render_template('evaluate.html')
+
+@app.route('/eval_result', methods=['POST', 'GET'])
+def process_form():
+
+    text = request.form['textinput']
+    file = request.files['file']
+
+    nlp_pred = None
+    cv_pred = None 
+    mn = None
+
+    if text and file:
+
+        d2v = Doc2VecModel()
+        cb = CBclassifier()
+        vectors = d2v.infer(text)
+        nlp_pred = cb.infer(vectors)
+
+        cv = CVmodel()
+        cv_pred = cv.infer(file)
+
+        mn = round(float(200/((1/cv_pred[:,1][0]) + (1/nlp_pred[:,1][0]))), 2)
+        nlp_pred = round(nlp_pred[:,1][0]*100, 2)
+        cv_pred = round(float(cv_pred[:,1][0]*100), 2)
+
+    
+    elif file and not text: 
+        cv = CVmodel()
+        cv_pred = cv.infer(file)
+        cv_pred = round(float(cv_pred[:,1][0]*100), 2)
+
+    
+    elif not file and text:
+        d2v = Doc2VecModel()
+        cb = CBclassifier()
+
+        vectors = d2v.infer(text)
+        nlp_pred = cb.infer(vectors)
+        nlp_pred = round(nlp_pred[:,1][0]*100, 2)
+
+
+    return render_template('eval_result.html', nlp_pred=nlp_pred, cv_pred=cv_pred, mn=mn)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/models.py
@@ -0,0 +1,123 @@
+import pandas as pd 
+import torch
+from gensim.models.doc2vec import Doc2Vec
+from models.preprocessing import text_preprocessing, image_preprocessing
+from catboost import CatBoostClassifier
+
+
+
+class Doc2VecModel:
+    def __init__(self, model_path='./models/doc2vec.model'):
+        self.model_path = model_path
+        self.model = self.load_model(model_path)
+
+    def load_model(self, model_path):
+        try:
+            model = Doc2Vec.load(model_path)
+            print(f"Model loaded successfully from {model_path}")
+            return model
+        except Exception as e:
+            print(f"Error loading model: {e}")
+            return None
+
+    def preprocess(self, text):
+        return text_preprocessing(text)
+
+    def infer(self, documents):
+
+        if self.model is None:
+            print("Model not loaded. Cannot perform inference.")
+            return None
+        
+
+        preprocessed_doc = self.preprocess(documents)
+        vectors = pd.DataFrame(self.model.infer_vector(doc.words) for doc in preprocessed_doc)
+        return vectors
+    
+    def train(self, hyperparameters):
+        pass
+
+
+
+class CBclassifier:
+    def __init__(self, model_path='./models/CB.cbm'):
+        self.model_path = model_path 
+        self.model = self.load_model(model_path)
+
+
+    def load_model(self, model_path):
+        try:
+            model = CatBoostClassifier().load_model(model_path)
+            print(f"Model loaded successfully from {model_path}")
+            return model
+        except Exception as e:
+            print(f"Error loading model: {e}")
+            return None
+        
+
+    def infer(self, vectors):
+
+        if self.model is None:
+            print("Model not loaded. Cannot perform inference.")
+            return None
+
+        preds = self.model.predict_proba(vectors)
+        return preds 
+    
+    
+    def train(self, hyperparameters):
+        pass
+
+
+
+class CVmodel:
+
+    def __init__(self, model_path='./models/cvResNet.pth'):
+        self.model_path = model_path
+        self.model = self.load_model(model_path)
+
+    def load_model(self, model_path):
+        try:
+            model = torch.load(model_path)
+            print(f"Model loaded successfully from {model_path}")
+            return model
+        except Exception as e:
+            print(f"Error loading model: {e}")
+            return None
+        
+        
+    def preprocess(self, path_to_image):
+
+        return image_preprocessing(path_to_image)
+    
+    def infer(self, path_to_image):
+
+        if self.model is None:
+            print("Model not loaded. Cannot perform inference.")
+            return None
+        
+        input = self.preprocess(path_to_image)
+
+        self.model.eval()
+        with torch.no_grad():
+            preds = torch.sigmoid(self.model(input))
+        
+        return preds
+
+
+    
+
+if __name__ == '__main__':
+    model = Doc2VecModel('./models/doc2vec.model')
+    text = pd.read_csv('/Users/nikitasenyatkin/Downloads/texts_labeled.tsv', sep='\t')
+    text = pd.Series(text['INPUT:comment'])[:3]
+    text = 'очень плохой чай не советую никому не хотим вам его предлагать'
+    d2v = model.infer(text)
+    cb = CBclassifier('./models/CB.cbm')
+    print(cb.infer(d2v))
+
+    cv = CVmodel('/Users/senia/Desktop/wb sphere analysis/wb-category-analysis/models/cvResNet.pth')
+    print(cv.infer('C:/Users/senia/Desktop/wb sphere analysis/images/0/50.jpg'))
+
+
+
